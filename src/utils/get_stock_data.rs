ed564eb0a2b489dfb71b37cedf2e9aa1b7b3154f
NeoPrint3D <drew@ronsman.com>
2024-01-27 19:38:30 -0600

    new stuff but still unreliable

--- src/utils/get_stock_data.rs
+++ src/utils/get_stock_data.rs
@@ -1,10 +1,48 @@
 use headless_chrome::protocol::cdp::Network::CookieParam;
-use headless_chrome::{Browser, LaunchOptionsBuilder};
+use headless_chrome::{Browser, LaunchOptionsBuilder, Tab};
 
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::error::Error;
 use std::fs;
+
+use std::sync::Arc;
+
+fn fetch_text(tab: &Arc<Tab>, selector: &str) -> Result<Option<String>, Box<dyn Error>> {
+    match tab.wait_for_element(selector) {
+        Ok(element) => match element.get_inner_text() {
+            Ok(text) => Ok(Some(text)),
+            Err(e) => {
+                eprintln!("Error getting inner text: {}", e);
+                Ok(None)
+            }
+        },
+        Err(_) => {
+            eprintln!("Element not found for selector: {}", selector);
+            Ok(None)
+        }
+    }
+}
+
+fn parse_value(
+    text: Option<String>,
+    index: usize,
+    trim_chars: &[char],
+) -> Result<Option<f32>, Box<dyn Error>> {
+    match text {
+        Some(t) => {
+            let value = t
+                .split_whitespace()
+                .nth(index)
+                .ok_or(format!("Failed to parse value at index: {}", index))?
+                .trim_matches(trim_chars)
+                .replace(",", "")
+                .parse::<f32>()?;
+            Ok(Some(value))
+        }
+        None => Ok(None), // No text to parse, so return None
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct StockMetrics {
@@ -44,76 +82,43 @@ pub fn fetch_stock_data() -> Result<(), Box<dyn Error>> {
     tab.navigate_to("https://finance.yahoo.com/portfolio/p_0/view/v1")?;
     tab.wait_until_navigated()?;
 
-    let total_holding = tab
-        .wait_for_element(
-            "div[data-yaft-module=\"tdv2-applet-fin-portfolio-gainloss\"] > div > span",
-        )?
-        .get_inner_text()
-        .expect("Unable to get total holding value");
+    let total_holding = fetch_text(
+        &tab,
+        "div[data-yaft-module=\"tdv2-applet-fin-portfolio-gainloss\"] > div > span",
+    );
+    let daily_gain = fetch_text(&tab, "div[data-yaft-module=\"tdv2-applet-fin-portfolio-gainloss\"] > div:nth-of-type(3) > span:nth-of-type(2)");
+    let total_gain = fetch_text(&tab, "div[data-yaft-module=\"tdv2-applet-fin-portfolio-gainloss\"] > div:nth-of-type(4) > span:nth-of-type(2)");
 
-    let daily_gain = tab.wait_for_element(
-        "div[data-yaft-module=\"tdv2-applet-fin-portfolio-gainloss\"] > div:nth-of-type(3) > span:nth-of-type(2)"
-    )?.get_inner_text().expect("Unable to get daily gain value");
+    let total_holding_value = parse_value(total_holding?, 0, &['$', ','])?;
+    let total_gain_value = parse_value(total_gain?, 0, &['$', ','])?;
+    let daily_gain_value = parse_value(daily_gain?, 0, &['$', ','])?;
+    let total_gain_percent_value = parse_value(total_gain?, 1, &['$', ',', '%'])?;
+    let daily_gain_percent_value = parse_value(daily_gain?, 1, &['$', ',', '%'])?;
 
-    let total_gain = tab.wait_for_element(
-        "div[data-yaft-module=\"tdv2-applet-fin-portfolio-gainloss\"] > div:nth-of-type(4) > span:nth-of-type(2)"
-    )?.get_inner_text().expect("Unable to get total gain value");
-
-    let total_holding_value = total_holding
-        .split_whitespace()
-        .nth(0)
-        .expect("conversion error 1")
-        .trim_matches('$')
-        .replace(",", "")
-        .parse::<f32>()?;
-    let daily_gain_percent_value = daily_gain
-        .split_whitespace()
-        .nth(1)
-        .expect("conversion error 2")
-        .trim_matches('(')
-        .trim_matches(')')
-        .trim_matches('%')
-        .parse::<f32>()?;
-
-    let daily_gain_value = daily_gain
-        .split_whitespace()
-        .nth(0)
-        .expect("conversion error 3")
-        .parse::<f32>()?;
-
-    let total_gain_value = total_gain
-        .split_whitespace()
-        .nth(0)
-        .expect("conversion error 4")
-        .parse::<f32>()?;
-
-    let total_gain_percent_value = total_gain
-        .split_whitespace()
-        .nth(1)
-        .expect("conversion error 5")
-        .trim_matches('(')
-        .trim_matches(')')
-        .trim_matches('%')
-        .parse::<f32>()?;
-
-    let metrics = StockMetrics {
-        total_holding_value,
-        daily_gain_percent_value,
-        daily_gain_value,
-        total_gain_value,
-        total_gain_percent_value,
-    };
-
-    println!("Metrics: {:?}", metrics);
-    // data is in src/data/metrics.json from where the program is run
-    let metrics_json_string = serde_json::to_string(&metrics)?;
-    // see if this is a production build or not
-    let metrics_path = if cfg!(debug_assertions) {
-        "src/data/metrics.json"
+    if total_holding_value.is_none()
+        && daily_gain_percent_value.is_none()
+        && daily_gain_value.is_none()
+        && total_gain_value.is_none()
+        && total_gain_percent_value.is_none()
+    {
+        println!("All metrics are None. Skipping file write.");
     } else {
-        "/etc/yahoo-finance-metrics/metrics.json"
-    };
+        let metrics = StockMetrics {
+            total_holding_value: total_holding_value.unwrap_or(0.0),
+            daily_gain_percent_value: daily_gain_percent_value.unwrap_or(0.0),
+            daily_gain_value: daily_gain_value.unwrap_or(0.0),
+            total_gain_value: total_gain_value.unwrap_or(0.0),
+            total_gain_percent_value: total_gain_percent_value.unwrap_or(0.0),
+        };
+        println!("Metrics: {:?}", metrics);
+        let metrics_json_string = serde_json::to_string(&metrics)?;
+        let metrics_path = if cfg!(debug_assertions) {
+            "src/data/metrics.json"
+        } else {
+            "/etc/yahoo-finance-metrics/metrics.json"
+        };
 
-    fs::write(metrics_path, metrics_json_string)?;
+        fs::write(metrics_path, metrics_json_string)?;
+    }
     Ok(())
 }
